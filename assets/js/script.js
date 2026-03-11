ea192bc2371b8cc09ca26f3c27ee4645e686488b
Deepak <deepakkumarnieit@gmail.com>
2026-03-11 23:22:13 +0530

    fix qr code size

--- assets/js/script.js
+++ assets/js/script.js
@@ -222,8 +222,8 @@ function generateChallan() {
     qrContainer.innerHTML = '';
     new QRCode(qrContainer, {
         text: verifyUrl,
-        width: 128,
-        height: 128,
+        width: 200,
+        height: 200,
         colorDark: "#000000",
         colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.M
